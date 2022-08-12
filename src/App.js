5da8c3429ad626e1ef21b39fc92e82293669f46f
AdminAkai <akatoirotech@gmail.com>
2022-08-12 02:45:36 -0400

    added app title, changed app title, added font

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField))
     return (
       <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox 
           className='monsters-search-box'
           placeholder='search monsters' 
