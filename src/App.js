18a411ec11229a54bf8e02dd1f7c973e3d71fdbc
AdminAkai <akatoirotech@gmail.com>
2022-08-12 21:21:49 -0400

    added title changes to test re-paints, added react 18 style of rendering App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   const { data: monsters } = useFetchData('https://jsonplaceholder.typicode.com/users')
 
   const [searchField, setSearchField] = useState('')
+  const [title, setTitle] = useState('')
   const [filteredMonsters, setFilteredMonsters] = useState(monsters)
 
   useEffect(() => {
@@ -24,14 +25,25 @@ const App = () => {
     setSearchField(() => searchString)
   }
 
+  const onTitleChange = (e) => {
+    const searchString = e.target.value.toLowerCase()
+    setTitle(() => searchString)
+  }
+
 
   return (
       <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
       <SearchBox 
         className='monsters-search-box'
         placeholder='search monsters' 
         onChangeHandler={onSearchChange} 
+      />
+      <br />
+      <SearchBox 
+        className='title-search-box'
+        placeholder='set title' 
+        onChangeHandler={onTitleChange} 
       />
       <CardList monsters={filteredMonsters} />
     </div>
